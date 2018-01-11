fbe73b9ab735a51d6aef49905c701a974c740ef3
Deden Fathurahman <dedenf@gmail.com>
2018-01-12 01:16:00 +0700

    add readme, adding line

--- post-commit.sh
+++ post-commit.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-if ! command -v ls .gitsnap >/dev/null; then
+if command -v ls .gitsnap >/dev/null; then
   printf "creating selfie folder..."
   mkdir .gitsnap
 fi
-COMMITID=''
+FILENAME=''
 if command -v git rev-parse HEAD  > /dev/null; then
-    COMMITID=$(date +%Y-%m-%d-%H-%M-%S)-FIRST
+    FILENAME=$(date +%Y-%m-%d-%H-%M-%s)_FIRST
 else
-    COMMITID=$(date +%Y-%m-%d-%H-%M-%S)-$(git rev-parse HEAD)
+    FILENAME=$(date +%Y-%m-%d-%H-%M-%S)_$(git rev-parse HEAD)
 fi
-
-FILENAME=$(date +%Y-%m-%d-%H-%M-%S)_$COMMITID
-
-ffmpeg -f avfoundation -video_size 1280x720 -framerate 30 -i "0" -vframes 1 .gitsnap/$FILENAME.jpg
+ffmpeg -f avfoundation -video_size 1280x720 -framerate 30 -i "0" -vframes 1 .gitsnap/$FILENAME.jpg -loglevel quiet >> /dev/null
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Hipster Commit
+Take a selfie if you commit and push your code to your repo
+
+### Selfie
+Selfie photo will be saved at your project directory at `.gitsnap` folder specifically
+
+### How to run
