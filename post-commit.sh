a0fd0f58fa5466d90af41a9736e6916c35155653
Deden Fathurahman <dedenf@gmail.com>
2018-01-12 00:54:49 +0700

    next update

--- post-commit.sh
+++ post-commit.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-#COMMITID=$(git rev-parse HEAD)
-#imagesnap -q -w 1 .gitsnap/$COMMITID.jpg
 if ! command -v ls .gitsnap >/dev/null; then
   printf "creating selfie folder..."
   mkdir .gitsnap
 fi
-COMMITID='lah'
+COMMITID=''
 if command -v git rev-parse HEAD  > /dev/null; then
     COMMITID=$(date +%Y-%m-%d-%H-%M-%S)-FIRST
 else
     COMMITID=$(date +%Y-%m-%d-%H-%M-%S)-$(git rev-parse HEAD)
 fi
 
-FILENAME=$(date +%Y-%m-%d-%H-%M-%S)-$COMMITID.png
-echo $FILENAME
+FILENAME=$(date +%Y-%m-%d-%H-%M-%S)_$COMMITID
 
-#ffmpeg -f avfoundation -video_size 1280x720 -framerate 30 -i "0" -vframes 1 .gitsnap/$COMMITID.jpg
+ffmpeg -f avfoundation -video_size 1280x720 -framerate 30 -i "0" -vframes 1 .gitsnap/$FILENAME.jpg
