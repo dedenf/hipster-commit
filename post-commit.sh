920081a02967339eb2e69a3c2308882f1d919637
Deden Fathurahman <dedenf@gmail.com>
2018-01-12 11:46:23 +0700

    push push push

--- post-commit.sh
+++ post-commit.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-if command -v ls .gitsnap >/dev/null; then
-  printf "creating selfie folder..."
+
+### start post commit ###
+if ! command -v ls .gitsnap >/dev/null; then
+  printf "creating selfie folder...\n"
   mkdir .gitsnap
 fi
+
 FILENAME=''
 if command -v git rev-parse HEAD  > /dev/null; then
     FILENAME=$(date +%Y-%m-%d-%H-%M-%s)_FIRST
 else
     FILENAME=$(date +%Y-%m-%d-%H-%M-%S)_$(git rev-parse HEAD)
 fi
+### take a selfie!
+printf "📸  trying to take your awesome selfie..... 📸\n"
 ffmpeg -f avfoundation -video_size 1280x720 -framerate 30 -i "0" -vframes 1 .gitsnap/$FILENAME.jpg -loglevel quiet >> /dev/null
